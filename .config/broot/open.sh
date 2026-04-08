de359795af5427c77f771ddaf8bd548e0d44cef9
Thomas Goulet <thomas.goulet@dfo-mpo.gc.ca>
2026-04-08 08:19:14 -0400

    ZelliJ update and new open script

--- .config/broot/open.sh
+++ .config/broot/open.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-zellij ac toggle-floating-panes
-zellij ac write-chars ":o $1${2:+:$2}"
-zellij ac write 13
-zellij ac toggle-floating-panes
+file="$1"
+
+tab_id=$(zellij action list-tabs -a \
+    | awk 'NR>1 && $4=="true" {print $1; exit}')
+
+editor_pane=$(zellij action list-panes -a \
+    | awk -v editor="$EDITOR" -v tid="$tab_id" \
+        'NR>1 && $1==tid && $7==editor {print $4; exit}')
+
+zellij action write-chars -p "$editor_pane" ":o $file"
+zellij action send-keys -p "$editor_pane" "Enter"
