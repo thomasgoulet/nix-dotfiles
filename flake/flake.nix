6a6b7c7e81f87af6a0b3d7a22903f663d0160025
Thomas Goulet <thomas.goulet@dfo-mpo.gc.ca>
2026-04-23 19:24:10 +0000

    Update backlog fetching to use flake directly

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,18 +1,23 @@
 {
   description = "Configuration for NixOs / WSL";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/25.11";
+   inputs = {
+     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+     nixpkgs-stable.url = "github:nixos/nixpkgs/25.11";
 
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+     flake-parts.url = "github:hercules-ci/flake-parts";
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
 
-  };
+     backlog-md = {
+       url = "github:MrLesk/Backlog.md";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+
+   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
--- flake/modules/custom-bins.nix
+++ flake/modules/custom-bins.nix
@@ -1,24 +1,9 @@
 { inputs, ... }:
 {
-  perSystem = { pkgs, ... }: {
+  perSystem = { pkgs, system, ... }: {
     packages = {
-      
-      backlog-md = pkgs.stdenv.mkDerivation {
-        pname = "backlog-md";
-        version = "1.44.0";
-        src = pkgs.fetchurl {
-          url = "https://registry.npmjs.org/backlog.md-linux-x64/-/backlog.md-linux-x64-1.44.0.tgz";
-          sha256 = "1zc2kfzpw5m80gjnjnp8x9hpqxrldghls1p8a64hiana9j7b45zy";
-        };
-        dontUnpack = true;
-        dontFixup = true;
-        installPhase = ''
-          mkdir -p $out/bin
-          tar -xzf $src package/backlog
-          cp package/backlog $out/bin/backlog
-          chmod +x $out/bin/backlog
-        '';
-      };
+
+      backlog-md = inputs.backlog-md.packages.${system}.default;
 
       oasdiff = pkgs.stdenv.mkDerivation {
         pname = "oasdiff";
@@ -27,11 +12,16 @@
           url = "https://github.com/oasdiff/oasdiff/releases/download/v1.11.7/oasdiff_1.11.7_linux_amd64.tar.gz";
           sha256 = "093j69a9s1d4wysz7jwfpfp934z00s311ynqykb6ykppclihbwcp";
         };
-        dontUnpack = true;
+
+        sourceRoot = ".";
+
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        buildInputs = [ pkgs.stdenv.cc.cc.lib ];
+
         installPhase = ''
           mkdir -p $out/bin
-          tar -xzvf $src
           cp oasdiff $out/bin/
+          chmod +x $out/bin/oasdiff
         '';
       };
 
