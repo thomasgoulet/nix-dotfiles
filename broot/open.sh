c17906b04a4bede96063eaf41ca65fa19624d533
Thomas Goulet <thomas.goulet@dfo-mpo.gc.ca>
2026-05-13 12:45:50 +0000

    Fix open.sh script

--- broot/open.sh
+++ broot/open.sh
@@ -6,8 +6,8 @@ tab_id=$(zellij action list-tabs -a \
     | awk 'NR>1 && $4=="true" {print $1; exit}')
 
 editor_pane=$(zellij action list-panes -a \
-    | awk -v editor="$EDITOR" -v tid="$tab_id" \
-        'NR>1 && $1==tid && $7==editor {print $4; exit}')
+    | awk -v tid="$tab_id" \
+        'NR>1 && $1==tid && $6=="EDITOR" {print $4; exit}')
 
 zellij action write-chars -p "$editor_pane" ":o $file"
 zellij action send-keys -p "$editor_pane" "Enter"
