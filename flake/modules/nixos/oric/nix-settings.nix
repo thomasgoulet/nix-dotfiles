4f1b51f595eb6481223ada5b44abe58ed6bbeabd
Thomas Goulet <thomas.goulet@dfo-mpo.gc.ca>
2026-07-23 16:55:45 +0000

    Automatic GC

--- flake/modules/nixos/oric/nix-settings.nix
+++ flake/modules/nixos/oric/nix-settings.nix
@@ -5,4 +5,11 @@
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "thomas" ];
   };
+
+  nix.gc = {
+    automatic = true;
+    persistent = true;
+    dates = "Fri *-*-* 06:00:00";
+    options = "--delete-older-than 7d";
+  };
 }
